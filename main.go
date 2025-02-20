a4e41ab5d3f662e927363cd70341688fe3a4aa01
matshp0 <matvii.shpyruk@gmail.com>
2025-02-20 18:59:10 +0200

    add formatted output

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -20,4 +21,15 @@ func solveEquation(a, b, c float64) (*float64, *float64) {
 }
 
 func main() {
+	a, b, c := 4.0, -5.0, 12.0
+	x1, x2 := solveEquation(a, b, c)
+	if x1 == nil {
+		fmt.Println("No roots found")
+		return
+	}
+	if x2 == nil {
+		fmt.Printf("Found one root: %f", *x1)
+		return
+	}
+	fmt.Printf("Found two roots: %f, %f", *x1, *x2)
 }
