3cd7d32461fbd9a2d3532ed5d42880c41d4c5bd9
matshp0 <matvii.shpyruk@gmail.com>
2025-02-20 22:57:57 +0200

    add input from file

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -27,6 +30,7 @@ func processInput() (float64, float64, float64) {
 	var result [3]float64
 	var err error
 	params := [3]string{"a", "b", "c"}
+	fmt.Printf("\033[0m")
 	fmt.Println("Enter equation parameters:")
 
 	for i, el := range params {
@@ -60,6 +64,32 @@ func solveEquation(a, b, c float64) (*float64, *float64) {
 	return &x1, &x2
 }
 
+func readParamsFromFile() (float64, float64, float64) {
+	var result [3]float64
+	path := os.Args[1]
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var line string
+	if scanner.Scan() {
+		line = scanner.Text()
+	}
+	params := strings.Split(line, " ")
+	if len(params) != 3 {
+		panic("invalid input")
+	}
+	for i := 0; i < 3; i++ {
+		result[i], err = validateFloat(params[i])
+		if err != nil {
+			panic(err)
+		}
+	}
+	return result[0], result[1], result[2]
+}
+
 func logOutput(x1, x2 *float64) {
 	if x1 == nil {
 		fmt.Println("No roots found")
@@ -74,8 +104,12 @@ func logOutput(x1, x2 *float64) {
 
 func main() {
 	var x1, x2 *float64
-	fmt.Printf("\033[0m")
-	a, b, c := processInput()
-	x1, x2 = solveEquation(a, b, c)
+	if len(os.Args) <= 1 {
+		a, b, c := processInput()
+		x1, x2 = solveEquation(a, b, c)
+	} else {
+		a, b, c := readParamsFromFile()
+		x1, x2 = solveEquation(a, b, c)
+	}
 	logOutput(x1, x2)
 }
