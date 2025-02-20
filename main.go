d78bee48bd53b976422a90a65679ced888e13754
matshp0 <matvii.shpyruk@gmail.com>
2025-02-20 19:56:24 +0200

    add input processing

--- main.go
+++ main.go
@@ -3,7 +3,47 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
+
+const (
+	Reset    = "\033[0m"
+	Bold     = "\033[1m"
+	Italic   = "\033[3m"
+	Green    = "\033[32m"
+	Combined = Reset + Bold + Italic + Green
+)
+
+func validateFloat(input string) (float64, error) {
+	val, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid number: %v", err)
+	}
+	return val, nil
+}
+
+func processInput() (float64, float64, float64) {
+	var arr [3]string
+	var result [3]float64
+	var err error
+	params := [3]string{"a", "b", "c"}
+	fmt.Println("Enter equation parameters:")
+
+	for i, el := range params {
+		for {
+			fmt.Printf(Reset+"%v = "+Combined, el)
+			fmt.Scan(&arr[i])
+			result[i], err = validateFloat(arr[i])
+			if err != nil {
+				fmt.Printf(Reset+"Expected a valid real number, got '%v' instead\n", arr[i])
+				continue
+			}
+			break
+		}
+	}
+	fmt.Print(Reset)
+	return result[0], result[1], result[2]
+}
 
 func solveEquation(a, b, c float64) (*float64, *float64) {
 	d := b*b - 4*a*c
@@ -21,7 +61,8 @@ func solveEquation(a, b, c float64) (*float64, *float64) {
 }
 
 func main() {
-	a, b, c := 4.0, -5.0, 12.0
+	fmt.Printf("\033[0m")
+	a, b, c := processInput()
 	x1, x2 := solveEquation(a, b, c)
 	if x1 == nil {
 		fmt.Println("No roots found")
